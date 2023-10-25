b47b2dd17e7f380e54800d400cb6211497106231
pehara <130656028+pehara@users.noreply.github.com>
2023-10-24 19:34:33 -0700

    Step 1

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
+// Step 0
+
 const gameName = "hehe game";
 
 document.title = gameName;
@@ -9,3 +11,18 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+// Step 1
+
+// Adding Canvas
+const canvas: HTMLCanvasElement = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+
+// Applying CSS styling to the canvas
+canvas.style.border = "1px solid black"; // Thin black border
+canvas.style.borderRadius = "8px"; // Optional rounded corners
+canvas.style.boxShadow = "2px 2px 8px rgba(0, 0, 0, 0.2)"; // Optional drop shadow
+
+// Append the canvas to the app container
+app.append(canvas);
